d3ebc95217b48d86c34fbf4ab14dde667f8df303
Grant <humphries.grant@gmail.com>
2021-08-27 12:14:46 +0100

    add option2, removed some of the superfluous code throughout, removed the loops in the stochastic CRM functions so that it can be run one line at a time

---
+++ scripts/BatchMode_Test.R
@@ -0,0 +1,44 @@
+# Running the CRM in batch mode
+
+library(readxl)
+library(foreach)
+library(tidyverse)
+library(doParallel)
+library(stochLAB)
+
+Turbines <- readxl::read_xlsx("../Collision_Risk_Modelling/data/Spreadsheet.xlsx",sheet = "TurbineParameters")
+Birds <- readxl::read_xlsx("../Collision_Risk_Modelling/data/Spreadsheet.xlsx",sheet = "BirdParameters")
+Ws_relation <- readxl::read_xlsx("../Collision_Risk_Modelling/data/Spreadsheet.xlsx",sheet = "WindSpeedRelation")
+
+
+Trow <- Turbines[1,]
+Brow <- Birds[1,]
+
+Scenario_id <- paste("Turbine_Scenario",Trow$Scenario,"Species_Scenario",Brow$Species_Scenario,sep="_")
+
+
+BirdDat <- Brow %>% select(Species:AvoidanceExtendedSD)
+CountDat <- Brow %>% select(Jan:DecSD)
+TurbineDat <- Trow
+Ws_relation_rotation <- Ws_relation %>% select(windSpeed,rotationSpeed)
+Ws_relation_pitch <- Ws_relation %>% select(windSpeed,bladePitch)
+
+
+test <- stochasticBand(results_folder = "C:/Temp/CRM_TEST/",scenario_id = Scenario_id,
+               BirdData = BirdDat,TurbineData = TurbineDat,
+               CountData = CountDat,CRSpecies = BirdDat$Species,TPower = TurbineDat$Tpower,
+               NTurbines = TurbineDat$NTurbines,LargeArrayCorrection = TRUE,
+               WFWidth = TurbineDat$Width,
+               Prop_Upwind = 0.5,
+               Latitude = TurbineDat$Latitude,
+               TideOff = TurbineDat$TidalOffset,
+               windSpeedMean = TurbineDat$windSpeedMean,
+               windSpeedSD = TurbineDat$windSpeedSD,
+               windData_rotation = Ws_relation_rotation,
+               windData_pitch = Ws_relation_pitch,
+               c_densOpt = "truncNorm"
+               )
+
+
+
+
