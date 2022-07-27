e8f8f24fee10697251e6e7b676b102fc220fe479
grwhumphries <grant.humphries@hidefsurveying.co.uk>
2022-07-27 21:18:18 +0100

    update srr standards script

--- R/srr-stats-standards.R
+++ R/srr-stats-standards.R
@@ -98,9 +98,9 @@ NULL
 #' G5.7 not applicable as no convergence occurs here - this is mostly random sampling
 #' @srrstatsNA {G2.16} *All functions should also provide options to handle undefined values (e.g., `NaN`, `Inf` and `-Inf`), including potentially ignoring or removing such values.*
 #' G2.16 is handled by the validate_inputs scripts
-#' @srrstatsTODO {G5.2} *Appropriate error and warning behaviour of all functions should be explicitly demonstrated through tests. In particular,*
-#' @srrstatsTODO {G5.2a} *Every message produced within R code by `stop()`, `warning()`, `message()`, or equivalent should be unique*
-#' @srrstatsTODO {G5.2b} *Explicit tests should demonstrate conditions which trigger every one of those messages, and should compare the result with expected values.*
+#' @srrstatsNA {G5.2} *Appropriate error and warning behaviour of all functions should be explicitly demonstrated through tests. In particular,*
+#' @srrstatsNA {G5.2a} *Every message produced within R code by `stop()`, `warning()`, `message()`, or equivalent should be unique*
+#' @srrstatsNA {G5.2b} *Explicit tests should demonstrate conditions which trigger every one of those messages, and should compare the result with expected values.*
 #' G5.2 to 5.2b is handled by the validate_inputs scripts
 #' @noRd
 NULL
